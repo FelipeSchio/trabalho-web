d82e414c91be951c7633e52582020488701b690c
FelipeSchio <felipe.schiocchet2002@gmail.com>
2022-11-23 17:20:38 -0300

    Rotas para as telas 2

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template
 from flask_restful import Api
-from resources.movies import Movies, Movie
 from resources.users import User, UserLogin
 from flask_jwt_extended import JWTManager
 
@@ -9,58 +8,54 @@ api = Api(app)
 jwt = JWTManager(app)
 
 # conexão com mysql
-DATABASE_URI = 'mysql+pymysql://root@localhost/felipe?charset=utf8mb4'
-app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-#conexão com postgres
-# DATABASE_URI = 'postgresql+psycopg2://postgres:admin@localhost:5432/dbpython'
+# DATABASE_URI = 'mysql+pymysql://root@localhost/felipe?charset=utf8mb4'
 # app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['JWT_SECRET_KEY'] = 'Senai2022'
 
-@app.before_first_request
-def create_database():
-    database.create_all()
+# app.config['JWT_SECRET_KEY'] = 'Senai2022'
+
+# @app.before_first_request
+# def create_database():
+    # database.create_all()
 
 # Telas
 @app.route("/")
 def login():
-    return "Login"
+    return render_template("login.html")
 
 @app.route("/menu")
 def menu():
-    return "Menu"
+    return render_template("menu.html")
 
 @app.route("/tabela")
 def tabela():
-    return "Tabela"
+    return render_template("tabela.html")
 
 @app.route("/caixa")
 def caixa():
-    return "caixa"
+    return render_template("caixa.html")
 
 @app.route("/produto")
 def produto():
-    return "Produto"
+    return render_template("produto.html")
 
 @app.route("/fornecedor")
 def fornecedor():
-    return "Fornecedor"
+    return render_template("fornecedor.html")
 
 @app.route("/funcionario")
 def funcionario():
-    return "Funcionario"
+    return render_template("funcionario.html")
 
 @app.route("/faq")
 def faq():
-    return "FAQ"
+    return render_template("faq.html")
 
 @app.route("/relatorio")
 def relatorio():
     return "Relatorio"
 
 if __name__ == '__main__':
-    from sql_alchemy import database
-    database.init_app(app)
+    # from sql_alchemy import database
+    # database.init_app(app)
     app.run(debug=True)
